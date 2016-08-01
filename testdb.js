c36507a11c114fa32f0b70268ab57306872c2301
agyenes <aron.gyenes@gmail.com>
2016-08-01 14:36:37 +0200

    html spaces deleteed, quote marks corrected, inline formatting deleted, 3 tests deleted

--- testdb.js
+++ testdb.js
@@ -5,25 +5,6 @@ var sinon = require('sinon');
 
 var meal = require('./db');
 
-tape('true', function(t) {
-  t.equal(true, true);
-  t.end();
-});
-
-tape('proba have been called', function(t) {
-  var callback = sinon.spy();
-  callback();
-  t.ok(callback.called);
-  t.end();
-});
-
-tape('proba have been called with 3', function(t) {
-  var callback = sinon.spy();
-  callback(3);
-  t.ok(callback.calledWith(3));
-  t.end();
-});
-
 tape('addmeal calls query', function (t) {
   var mockConnection = {
     query: sinon.spy()
