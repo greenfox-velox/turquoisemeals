63dba4454db8574498ffc4f0508c2ab765acc5b4
Pfeter <peter.foldvary@gmail.com>
2016-08-02 13:32:18 +0200

    Clear code of connect.js based on eslint-config-airbnb-es5.

--- connect.js
+++ connect.js
@@ -3,13 +3,13 @@ var config = require('./CONFIG');
 var mysql = require('mysql');
 var connection = mysql.createConnection(config.sqlEntry);
 
-connection.connect(function(err){
-  if(err){
-    console.log("Error connecting to Db");
+connection.connect(function(err) {
+  if (err) {
+    console.log('Error connecting to Db');
     console.log(err);
     return;
   }
-  console.log("Connection established");
+  console.log('Connection established');
 });
 
 module.exports.connection = connection;
