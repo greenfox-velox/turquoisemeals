9271f0ecdb9ab2736b1affbeb0eefd216ecccd63
Pfeter <peter.foldvary@gmail.com>
2016-08-02 13:10:21 +0200

    Clear code based on eslint-config-airbnb-es5.

--- db.js
+++ db.js
@@ -1,43 +1,42 @@
-var Meals = (function (connection) {
-
+var Meals = function(connection) {
   function errorHandler(err) {
-    if(err) {
+    if (err) {
       console.log(err.toString());
       return;
     }
   }
 
-  function publicAddMeal (meal, cb) {
-    connection.query('INSERT INTO meals SET ?', meal, function(err,row){
+  function publicAddMeal(meal, cb) {
+    connection.query('INSERT INTO meals SET ?', meal, function(err, row) {
       errorHandler(err);
       if (row !== undefined && row.affectedRows) {
-        cb({"status": "ok", "meal": {"id": row.insertId, "name": meal.name, "calories": meal.calories, "date": meal.date}});
+        cb({'status': 'ok', 'meal': {'id': row.insertId, 'name': meal.name, 'calories': meal.calories, 'date': meal.date}});
       } else {
         cb(row);
       }
     });
   }
 
-  function publicGetMeal (cb) {
-    connection.query('SELECT * FROM meals;',function(err,rows){
+  function publicGetMeal(cb) {
+    connection.query('SELECT * FROM meals;', function(err, rows) {
       errorHandler(err);
-      cb({"meals":rows});
+      cb({'meals': rows});
     });
   }
 
-  function publicDelMeal (id, cb) {
-    connection.query('UPDATE meals SET deleted = true WHERE id = ?', id, function(err,row){
+  function publicDelMeal(id, cb) {
+    connection.query('UPDATE meals SET deleted = true WHERE id = ?', id, function(err, row) {
       errorHandler(err);
       if (row.affectedRows) {
-        cb({"status": "ok", "meal": {"id": id}});
+        cb({'status': 'ok', 'meal': {'id': id}});
       } else {
-        cb({"status": "not exists"});
+        cb({'status': 'not exists'});
       }
     });
   }
 
-  function publicFilterMeals (date, cb) {
-    connection.query('SELECT * FROM meals WHERE meals.date LIKE ?;', date + '%', function(err,rows){
+  function publicFilterMeals(date, cb) {
+    connection.query('SELECT * FROM meals WHERE meals.date LIKE ?;', date + '%', function(err, rows) {
       errorHandler(err);
       cb(rows);
     });
@@ -49,6 +48,6 @@ var Meals = (function (connection) {
     delMeal: publicDelMeal,
     filterMeals: publicFilterMeals
   };
-});
+};
 
 module.exports = Meals;
