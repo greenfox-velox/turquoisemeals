56449f4f65893a90a27e740482a8465cc19f1849
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-08-04 16:52:17 +0200

    refactor karma tests

--- test/unit/testingAngularUnitSpec.js
+++ test/unit/testingAngularUnitSpec.js
@@ -18,25 +18,15 @@ describe('mealController', function() {
     var $controller = $injector.get('$controller');
 
     createController = function() {
-     scope = $rootScope.$new();
-     return $controller('mealController', {'$scope' : scope });
+      scope = $rootScope.$new();
+      return $controller('mealController', {'$scope' : scope });
     };
   }));
-
 
   afterEach(function() {
     $httpBackend.verifyNoOutstandingExpectation();
     $httpBackend.verifyNoOutstandingRequest();
   });
-
-
-  it('should fetch authentication token', function() {
-    $httpBackend.expectGET('http://localhost:3000/meals');
-    var controller = createController();
-    $httpBackend.flush();
-  // expect($rootScope.status).toBeDefined();
-  });
-
 
   it('should get 1 meal from the with $http', function() {
     $httpBackend.expectGET('http://localhost:3000/meals');
@@ -55,19 +45,23 @@ describe('mealController', function() {
     expect(scope.newMeal).toBeDefined();
   });
 
-  // it('should add 1 meal to the meal list and clear input fields after', function() {
-  //   $httpBackend.flush();
-  //   scope.sumcalories = 0;
-  //   scope.newMeal.name = 'bread';
-  //   scope.newMeal.calories = 125;
-  //   scope.newMeal.date = new Date(1982, 5, 26, 10, 20);
-  //   scope.addMeal();
-  //   expect(scope.meals.length).toBe(1);
-  //   expect(scope.meals[0].name).toBe('apple');
-  //   expect(scope.meals[0].calories).toBe(25);
-  //   expect(scope.meals[0].date).toEqual(new Date(1982, 5, 26, 10, 20));
-  //   expect(scope.meals[0].deleted).toBe(false);
-  // });
-  // });
-
+  it('should add 1 meal to the meal list and clear input fields after', function() {
+    scope.sumcalories = 0;
+    scope.newMeal.name = 'bread';
+    scope.newMeal.calories = 125;
+    scope.newMeal.date = new Date(1982, 5, 26, 10, 20);
+    scope.addMeal();
+    expect(scope.meals.length).toBe(2);
+    expect(scope.meals[0].name).toBe('apple');
+    expect(scope.meals[0].calories).toBe(25);
+    expect(scope.meals[0].date).toEqual(new Date(1982, 5, 26, 10, 20));
+    expect(scope.meals[0].deleted).toBe(false);
+    expect(scope.meals[1].name).toBe('bread');
+    expect(scope.meals[1].calories).toBe(125);
+    expect(scope.meals[1].date).toEqual('1982-6-26 10-20');
+    expect(scope.meals[1].deleted).toBe(false);
+    scope.newMeal.name = '';
+    scope.newMeal.calories = '';
+    scope.newMeal.date = new Date(1982, 5, 26, 10, 20);
+  });
 });
