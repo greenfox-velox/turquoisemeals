9a483e938169564a8dc002e1ad616d7a4d20341f
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-08-09 10:44:39 +0200

    add karma delete test

--- test/unit/testingAngularUnitSpec.js
+++ test/unit/testingAngularUnitSpec.js
@@ -15,19 +15,12 @@ describe('mealController', function() {
     authRequestHandler = $httpBackend.when('POST', 'http://localhost:3000/meals')
                           .respond({'meal': [{id: 5, name: 'egg', calories: 25, date: new Date(1982, 5, 26, 10, 20), deleted: false}]});
     // Get hold of a scope (i.e. the root scope)
+    authRequestHandler = $httpBackend.when('DELETE', 'http://localhost:3000/meals/5')
+                          .respond({});
+    // Get hold of a scope (i.e. the root scope)
     $rootScope = $injector.get('$rootScope');
     // The $controller service is used to create instances of controllers
     var $controller = $injector.get('$controller');
-
-    // // Set up the mock http service responses
-    // $httpBackend = $injector.post('$httpBackend');
-    // // backend definition common for all tests
-    // authRequestHandler = $httpBackend.when('POST', 'http://localhost:3000/meals', {name: 'bread', calories: 50, date: new Date(1982, 5, 26, 10, 20), deleted: false})
-    //                       .respond({'meal': [{id: 5, name: 'bread', calories: 50, date: new Date(1982, 5, 26, 10, 20), deleted: false}]});
-    // // Get hold of a scope (i.e. the root scope)
-    // $rootScope = $injector.post('$rootScope');
-    // // The $controller service is used to create instances of controllers
-    // var $controller = $injector.post('$controller');
 
     createController = function() {
       scope = $rootScope.$new();
@@ -87,18 +80,23 @@ describe('mealController', function() {
     scope.addMeal();
     $httpBackend.expectPOST('http://localhost:3000/meals', JSON.stringify({name: 'egg', calories: 25, date: '1982-6-26 10-20', deleted: false}));
     $httpBackend.flush();
-    // expect(scope.meals).toBeDefined();
-    // expect(scope.meals.length).toBe(13);
-  // it('should add 1 meal with $http', function() {
-  //   $httpBackend.expectPOST('http://localhost:3000/meals', {name: 'bread', calories: 50, date: new Date(1982, 5, 26, 10, 20), deleted: false});
-  //   var controller = createController();
-  //   $httpBackend.flush();
-  //   // expect(scope.meals).toBeDefined();
-  //   // expect(scope.meals.length).toBe(1);
-  //   // expect(scope.meals[0].id).toBe(1);
-  //   // expect(scope.meals[0].name).toBe('apple');
-  //   // expect(scope.meals[0].calories).toBe(25);
-  //   // expect(scope.meals[0].date).toEqual(new Date(1982, 5, 26, 10, 20));
-  //   // expect(scope.meals[0].deleted).toBe(false);
+    expect(scope.meals.length).toBe(1);
+  });
+
+  it('should delete 1 meal with $http', function() {
+    $httpBackend.expectGET('http://localhost:3000/meals');
+    var controller = createController();
+    scope.meals = [];
+    expect(scope.meals.length).toBe(0);
+    scope.newMeal.name = 'egg';
+    scope.newMeal.calories = 25;
+    scope.newMeal.date = new Date(1982, 5, 26, 10, 20);
+    scope.addMeal();
+    expect(scope.meals.length).toBe(1);
+    scope.meals[scope.meals.length - 1].id = 5;
+    scope.removeMeal({name : 'egg', calories : 25, date : '1982-6-26 10-20', deleted : false, id : 5});
+    $httpBackend.expectDELETE('http://localhost:3000/meals/5');
+    expect(scope.meals.length).toBe(0);
+    $httpBackend.flush();
   });
 });
