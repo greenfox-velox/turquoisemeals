a98d87a5edb37df41196564b9cfec8052aa9dd1b
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-08-09 11:47:51 +0200

    add config

---
+++ CONFIG.js
@@ -0,0 +1,15 @@
+var sqlEntry = {
+  database: 'sql7130714',
+  host: 'sql7.freemysqlhosting.net',
+  port: '3306',
+  user: 'sql7130714',
+  password: 'SHJx3IubQ7'
+};
+// var sqlEntry = {
+//   database: 'caloriecounter',
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: 'velox'
+// };
+
+module.exports.sqlEntry = sqlEntry;
