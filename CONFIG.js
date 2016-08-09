fa1b208fcbf452ee126bf7b51d10871a246d4030
Öreg Zoltán <oreg_zoltan@yahoo.com>
2016-08-09 12:10:55 +0200

    Delete CONFIG.js

--- CONFIG.js
+++
@@ -1,9 +0,0 @@
-var sqlEntry = {
-  database: 'sql7130714',
-  host: 'sql7.freemysqlhosting.net',
-  port: '3306',
-  user: 'sql7130714',
-  password: 'SHJx3IubQ7'
-};
-
-module.exports.sqlEntry = sqlEntry;
